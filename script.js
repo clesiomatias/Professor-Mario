dea204e9e58006efcf904b39d43475644c436642
clesiomatias <clesiofmatias@gmail.com>
2021-12-01 11:41:33 -0300

    XIII

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 var divArtigos = document.querySelector(".artigos");
+var ultimo = document.querySelector(".ultimo-artigo");
+const post = (endereco) => {
+  var resp;
+  let requestURL = endereco;
+  let request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    let resposta = request.response;
+    console.log(resposta.files[0].filename);
+    console.log(resposta.files[0].blob_url);
+  };
+};
+
+const ultimoArtigo = () => {
+  let requestURL = "https://api.github.com/users/ProfMLE/events";
+  let request = new XMLHttpRequest();
+  request.open("GET", requestURL);
+  request.responseType = "json";
+  request.send();
+
+  request.onload = function () {
+    let conteudo = request.response;
+    let elementos = conteudo.filter(
+      (item) => item.repo.name == "ProfMLE/Rep01"
+    );
+    let primeiro = elementos.filter(
+      (item) => item.payload.commits[0].message == "Add files via upload"
+    )[0].payload.commits[0].url;
+    post(primeiro);
+  };
+};
 
 const req = () => {
+  ultimoArtigo();
   var requestURL = "https://api.github.com/repos/ProfMLE/Rep01/contents/";
   var request = new XMLHttpRequest();
   request.open("GET", requestURL);
