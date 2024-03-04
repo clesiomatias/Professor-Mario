b37ebc2131f07d7afb298f0b4874cf961cd1ac4c
clesiomatias <clesiofmatias@gmail.com>
2024-03-04 19:28:39 -0300

    correcoes

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
+var divArtigos = document.querySelector(".artigos");
+var ultimo = document.querySelector(".ultimo-artigo");
+
 const post = (endereco) => {
   let requestURL = endereco;
   let request = new XMLHttpRequest();
   request.open("GET", requestURL);
   request.responseType = "json";
+  request.send();
 
   request.onload = function () {
-    if (request.status === 200) {
-      let resposta = request.response;
-      if (resposta && resposta.files && resposta.files.length > 0) {
-        let nome = resposta.files[0].filename.split(".").shift();
-        let direcao = resposta.files[0].blob_url;
-        let elemento = document.createElement("a");
-        elemento.innerHTML = nome;
-        elemento.href = direcao;
-        elemento.target = "_blank";
-        let ultimo_elemento = document.createElement("a");
-        ultimo_elemento.innerHTML = nome;
-        ultimo_elemento.href = `https://github.com/ProfMLE/Rep01/blob/master/${nome}.pdf`;
-        ultimo_elemento.target = "_blank";
-        ultimo.appendChild(ultimo_elemento);
-      }
-    } else {
-      console.error("Erro na requisição: " + request.status);
-    }
+    let resposta = request.response;
+    let nome = resposta.files[0].filename.split(".").shift();
+    let direcao = resposta.files[0].blob_url;
+    let elemento = document.createElement("a");
+    elemento.innerHTML = nome;
+    elemento.href = direcao;
+    elemento.target = "_blank";
+    let ultimo_elemento = document.createElement("a");
+    ultimo_elemento.innerHTML = nome;
+    ultimo_elemento.href = `https://github.com/ProfMLE/Rep01/blob/master/${nome}.pdf`;
+    ultimo_elemento.target = "_blank";
+    ultimo.appendChild(ultimo_elemento);
   };
-
-  request.onerror = function () {
-    console.error("Erro na requisição.");
-  };
-
-  request.send();
 };
 
 const ultimoArtigo = () => {
@@ -37,28 +29,19 @@ const ultimoArtigo = () => {
   let request = new XMLHttpRequest();
   request.open("GET", requestURL);
   request.responseType = "json";
+  request.send();
 
   request.onload = function () {
-    if (request.status === 200) {
-      let conteudo = request.response;
-      let elementos = conteudo.filter(
-        (item) => item.repo.name == "ProfMLE/Rep01"
-      );
-      let primeiro = elementos.filter(
-        (item) => item.payload.commits[0].message == "Add files via upload"
-      )[0].payload.commits[0].url;
+    let conteudo = request.response;
+    let elementos = conteudo.filter(
+      (item) => item.repo.name == "ProfMLE/Rep01"
+    );
+    let primeiro = elementos.filter(
+      (item) => item.payload.commits[0].message == "Add files via upload"
+    )[0].payload.commits[0].url;
 
-      post(primeiro);
-    } else {
-      console.error("Erro na requisição: " + request.status);
-    }
+    post(primeiro);
   };
-
-  request.onerror = function () {
-    console.error("Erro na requisição.");
-  };
-
-  request.send();
 };
 
 const req = () => {
@@ -67,31 +50,21 @@ const req = () => {
   var request = new XMLHttpRequest();
   request.open("GET", requestURL);
   request.responseType = "json";
+  request.send();
 
   request.onload = function () {
-    if (request.status === 200) {
-      var conteudos = request.response;
-      var nomes = conteudos.map((item) => item.name);
+    var conteudos = request.response;
+    var nomes = conteudos.map((item) => item.name);
 
-      nomes.map((item) => {
-        let elemento = document.createElement("a");
-        elemento.innerHTML = item.split(".").shift();
-        elemento.href = `https://github.com/ProfMLE/Rep01/blob/master/${item}`;
-        elemento.target = "_blank";
-        divArtigos.appendChild(elemento);
-      });
-    } else {
-      console.error("Erro na requisição: " + request.status);
-    }
+    nomes.map((item) => {
+      let elemento = document.createElement("a");
+      elemento.innerHTML = item.split(".").shift();
+      elemento.href = `https://github.com/ProfMLE/Rep01/blob/master/${item}`;
+      elemento.target = "_blank";
+      divArtigos.appendChild(elemento);
+    });
   };
-
-  request.onerror = function () {
-    console.error("Erro na requisição.");
-  };
-
-  request.send();
 };
-
 const updateCurrentDate = () => {
   var currentYear = new Date().getFullYear();
   var spanElement = document.querySelector(".current-date");
